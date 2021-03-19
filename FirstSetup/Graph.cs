df85f81f614dc49de733d288fa4df50d5089431d
Muhammad Tito P <cocabc1@gmail.com>
2021-03-19 21:20:56 +0700

    first init, ctor of Graph

---
+++ FirstSetup/Graph.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphConsole
+{
+    class Graph
+    {
+        private Dictionary<string, List<string>> graphDict;  //menyimpan dictionary edges dengan key adalah node dan valuenya adalah nama node yang ia terhubung
+        private int countEdges;                 //menyimpan ada berapa edge
+        private int countVertices;              //menyimpan ada berapa vertice
+
+        //ctor tp langsung aja masukin ada berapa edge dan apa aja
+        public Graph(int countEdges, string[] rawEdges)
+        {
+            this.countEdges = countEdges;
+            this.countVertices = 0;
+
+            graphDict = new Dictionary<string, List<string>>();
+            for (int i = 0; i < rawEdges.Length; i++)
+            {
+                string rawKey = rawEdges[i].Split(" ")[0];
+                string rawValue = rawEdges[i].Split(" ")[1];
+
+                if (graphDict.ContainsKey(rawKey))
+                {
+                    graphDict[rawKey].Add(rawValue);
+                }
+                else
+                {
+                    List<string> temp = new List<string>();
+                    temp.Add(rawValue);
+                    graphDict.Add(rawKey, temp);
+                    this.countVertices++;
+                }
+            }
+        }
+        
+        public void printAll()
+        {
+            foreach (KeyValuePair<string, List<string>> entry in graphDict)
+            {
+                Console.Out.Write("Key: "+ entry.Key +" Value: ");
+                foreach(string str in entry.Value)
+                {
+                    Console.Out.Write(str + " ");
+                }
+                Console.Out.WriteLine();
+            }
+        }
+
+        public List<string> exploreFriendBFS()
+        {
+            List<string> retVal = new List<string>();
+            return retVal;
+        }
+        public List<string> exploreFriendDFS()
+        {
+            List<string> retVal = new List<string>();
+            return retVal;
+        }
+        public List<string> friendRecommendationBFS()
+        {
+            List<string> retVal = new List<string>();
+            return retVal;
+        }
+        public List<string> friendRecommendationDFS()
+        {
+            List<string> retVal = new List<string>();
+            return retVal;
+        }
+
+    }
+}
---
+++ FirstSetup/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GraphConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] temp = { "A B", "A C", "A D", "B C", "B E", "B F" };
+            Graph G = new Graph(6, temp);
+            G.printAll();
+        }
+    }
+}
