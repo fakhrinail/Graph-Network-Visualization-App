c8dc1ecbf598be4f6a282de2b2c5c53be995c1a2
habibinaarif <habibina.arif@gmail.com>
2021-03-21 15:06:10 +0700

    add feature Explore Friend BFS

--- FirstSetup/Program.cs
+++ FirstSetup/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GraphConsole
 {
@@ -9,6 +10,49 @@ namespace GraphConsole
             string[] temp = { "A B", "A C", "A D", "B C", "B E", "B F" };
             Graph G = new Graph(6, temp);
             G.printAll();
+            Console.Out.WriteLine();
+
+            // Explore Friend BFS
+            List<string> path = new List<string>();
+            string awal = "A";
+            string tujuan = "F";
+            path = G.exploreFriendBFS(awal, tujuan);
+
+            Console.Out.WriteLine("Nama akun: "+awal+" dan "+tujuan);
+
+            if (path.Count > 0)
+            {
+                int n = path.Count - 2;
+                Console.Write(n);
+                if (n % 10 == 1 && (n % 100 < 10 || n % 100 > 20))
+                {
+                    Console.Write("st");
+                }
+                else if (n % 10 == 2 && (n % 100 < 10 || n % 100 > 20))
+                {
+                    Console.Write("nd");
+                }
+                else if (n % 10 == 3 && (n % 100 < 10 || n % 100 > 20))
+                {
+                    Console.Write("rd");
+                }
+                else
+                {
+                    Console.Write("th");
+                }
+                Console.Out.WriteLine("-degree connection");
+                for (int i = 0; i < path.Count - 1; i++)
+                {
+                    Console.Write(path[i] + " --> ");
+                }
+                Console.WriteLine(path[path.Count - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Tidak ada jalur koneksi yang tersedia");
+                Console.WriteLine("Anda harus memulai koneksi baru itu sendiri.");
+            }
+            
         }
     }
 }
